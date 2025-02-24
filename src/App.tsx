d45ab60e7c17fb3e9e5a0c26e4c8ea010cdf41ec
Paul Ryan <paul.ryan@quantcast.com>
2025-02-24 12:13:57 -0800

    Remove unused style imports.

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <div>
+      <div>
+        <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
           <div className="text-xl font-medium text-black dark:text-white">
             Paul Ryan
           </div>
