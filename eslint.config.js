e2bb4094dbb6127880f7813b59909ff1260ddb39
Paul Ryan <paul.ryan@quantcast.com>
2025-02-24 11:22:41 -0800

    Fix eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat.jsxRuntime,
-  eslintConfigPrettier, 
-  eslintConfigPrettier.configs.recommended,
-  eslintConfigPrettier.configs.recommendedTypescript,
-  eslintConfigPrettier.configs.recommendedReact,
-  eslintConfigPrettier.configs.recommendedReactTypescript,
-  eslintConfigPrettier.configs.recommendedJsxRuntime,
-  eslintConfigPrettier.configs.recommendedReactJsxRuntime,
-  eslintConfigPrettier.configs.recommendedReactTypescriptJsxRuntime,
+  eslintConfigPrettier,
+  {
+    settings: { react: { version: "^19.0.0" } },
+    plugins: {
+      react,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+    },
+  },
 ];
