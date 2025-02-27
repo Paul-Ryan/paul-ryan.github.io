f66d8ae07489e3ca26d10d8626c68fb0810a0455
Paul Ryan <paul.ryan@quantcast.com>
2025-02-27 15:59:00 -0800

    Just messing around

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,46 @@ function App() {
 
   return (
     <>
-      <div>
-        <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-          <div className="text-xl font-medium text-black dark:text-white">
-            Paul Ryan
-          </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            <p>Hello.</p>
-            <p>
-              I’m Paul, a web developer focused on frontend development with{" "}
-              {xYearsExperience}. I’ve spent the last few years working on
-              small, motivated teams to create useful products with thoughtful
-              user interfaces.
-            </p>
+      {/* <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"> */}
+      <div className="flex items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <div className="text-xl font-medium text-black dark:text-white">
+          <p>Paul Ryan</p>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            Web Developer
+            <br />
+            Cat Owner
+          </p>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            <a
+              href="https://github.com/Paul-Ryan"
+              className="text-gray-500 dark:text-gray-400"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://github.com/Paul-Ryan
+            </a>
+          </p>
+        </div>
+        <div className="text-gray-500 dark:text-gray-400">
+          <p>Hello.</p>
+          <p>
+            I’m Paul, a web developer focused on frontend development with{" "}
+            {xYearsExperience}. I’ve spent the last few years working on small,
+            motivated teams to create useful products with thoughtful user
+            interfaces.
+          </p>
 
-            <p>
-              These are the technologies I’ve been working with lately, but I’m
-              very capable of picking up new skills depending on the
-              circumstances and product requirements.
-            </p>
+          <p>
+            These are the technologies I’ve been working with lately, but I’m
+            very capable of picking up new skills depending on the circumstances
+            and product requirements.
+          </p>
 
-            <p>
-              Here are a few of the projects I’ve been working on my own. I also
-              have plenty of experience working on corporate products, but I
-              hope these examples give you a little bit of an idea about what I
-              find interesting and cool.
-            </p>
+          <p>
+            Here are a few of the projects I’ve been working on my own. I also
+            have plenty of experience working on corporate products, but I hope
+            these examples give you a little bit of an idea about what I find
+            interesting and cool.
           </p>
         </div>
       </div>
