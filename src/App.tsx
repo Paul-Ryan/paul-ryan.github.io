a44ef072c4647612990f459931541bf2a80734f0
Paul Ryan <paul.ryan@quantcast.com>
2025-03-02 14:28:27 -0800

    Basic styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,37 @@
 import "./App.css";
 
 function App() {
-  const startDate = new Date("2018-06-01");
-  const currentDate = new Date();
-  const xYearsExperience = `${currentDate.getFullYear() - startDate.getFullYear()} years experience`;
-
   return (
-    <>
-      {/* <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"> */}
-      <div className="flex items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <div className="text-xl font-medium text-black dark:text-white">
-          <p>Paul Ryan</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            Web Developer
+    <div className="flex h-screen flex-col justify-center align-middle">
+      <div className="flex max-w-4xl grow flex-col sm:flex-row">
+        <section className="flex flex-col justify-center p-2 sm:grow-1 sm:p-4">
+          <h1>
+            Hello,
             <br />
-            Cat Owner
-          </p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            <a
-              href="https://github.com/Paul-Ryan"
-              className="text-gray-500 dark:text-gray-400"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://github.com/Paul-Ryan
+            I&apos;m Paul
+          </h1>
+          <p>/ Web Developer</p>
+        </section>
+        <section className="mt-2 flex flex-col justify-center border-t-2 p-2 sm:mt-0 sm:grow-1 sm:border-t-0 sm:p-4 sm:pt-0">
+          <h1>
+            <a href="https:pauls-movie-list.com" rel="noopener noreferrer">
+              Paul&apos;s Movie List
             </a>
-          </p>
-        </div>
-        <div className="text-gray-500 dark:text-gray-400">
-          <p>Hello.</p>
-          <p>
-            I’m Paul, a web developer focused on frontend development with{" "}
-            {xYearsExperience}. I’ve spent the last few years working on small,
-            motivated teams to create useful products with thoughtful user
-            interfaces.
-          </p>
-
-          <p>
-            These are the technologies I’ve been working with lately, but I’m
-            very capable of picking up new skills depending on the circumstances
-            and product requirements.
-          </p>
-
-          <p>
-            Here are a few of the projects I’ve been working on my own. I also
-            have plenty of experience working on corporate products, but I hope
-            these examples give you a little bit of an idea about what I find
-            interesting and cool.
-          </p>
-        </div>
+          </h1>
+        </section>
       </div>
-    </>
+      <footer className="flex gap-4 self-end justify-self-end p-4">
+        <a
+          href="https://www.linkedin.com/in/paul-ryan-12a5932a/"
+          rel="noopener noreferrer"
+        >
+          linkedin
+        </a>
+        <a href="https://github.com/Paul-Ryan" rel="noopener noreferrer">
+          github
+        </a>
+      </footer>
+    </div>
   );
 }
 
